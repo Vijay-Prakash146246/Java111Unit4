72e88b01f5eaf7654572ebd4dfb24b3498fad94e
Vijay-Prakash146246 <vp146246@gmail.com>
2022-05-18 13:32:18 +0530

    adding all assignment

---
+++ AssignmentQ3.java
@@ -0,0 +1,15 @@
+package com.vijay;
+
+public class AssignmentQ3 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		sum(5);
+
+	}
+	static void sum(int num)
+	{
+		int sum = (num*(num+1))/2;
+		System.out.println(sum);
+	}
+
+}
